ec3a0bd18e40d096ad6dc43ba202b19c6b393f3a
Clemens Winter <clemenswinter1@gmail.com>
2021-10-27 21:12:26 -0700

    Parse dataclasses

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ use pyo3::types::{PyDict, PyList, PyTuple};
 use pyo3::{exceptions, prelude::*, wrap_pyfunction};
 
 #[pyfunction()]
-pub fn to_string(_py: Python, value: &PyAny) -> PyResult<String> {
-    let value = extract(value)?;
-    println!("{:?}", value);
+pub fn to_string(py: Python, value: &PyAny) -> PyResult<String> {
+    let value = extract(py, value)?;
     value
         .to_string_pretty(ron::ser::PrettyConfig::default().struct_names(true))
         .map_err(|e| exceptions::PyValueError::new_err(format!("{}", e)))
@@ -16,27 +15,27 @@ fn pyron(_py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
-fn extract(value: &PyAny) -> Result<ron::Value, PyErr> {
+fn extract(py: Python, value: &PyAny) -> Result<ron::Value, PyErr> {
     if let Ok(dict) = value.cast_as::<PyDict>() {
         let mut map = ron::Map::new();
         for (key, value) in dict {
-            map.insert(extract(key)?, extract(value)?);
+            map.insert(extract(py, key)?, extract(py, value)?);
         }
         Ok(ron::Value::Map(map))
     } else if let Ok(tuple) = value.cast_as::<PyTuple>() {
         if is_namedtuple(tuple) {
-            extract_namedtuple(tuple)
+            extract_namedtuple(py, tuple)
         } else {
             let mut seq = vec![];
             for value in tuple.iter() {
-                seq.push(extract(value)?);
+                seq.push(extract(py, value)?);
             }
             Ok(ron::Value::Seq(seq))
         }
     } else if let Ok(list) = value.cast_as::<PyList>() {
         let mut seq = vec![];
         for value in list.iter() {
-            seq.push(extract(value)?);
+            seq.push(extract(py, value)?);
         }
         Ok(ron::Value::Seq(seq))
     } else if let Ok(str) = value.extract::<String>() {
@@ -47,6 +46,11 @@ fn extract(value: &PyAny) -> Result<ron::Value, PyErr> {
         Ok(ron::Value::Number(ron::Number::from(float)))
     } else if let Ok(bool) = value.extract::<bool>() {
         Ok(ron::Value::Bool(bool))
+    } else if PyModule::import(py, "dataclasses")?
+        .call_method1("is_dataclass", (value,))?
+        .extract::<bool>()?
+    {
+        extract_dataclass(py, value)
     } else {
         Err(exceptions::PyValueError::new_err(format!(
             "Unsupported type: {}",
@@ -58,20 +62,13 @@ fn extract(value: &PyAny) -> Result<ron::Value, PyErr> {
 fn is_namedtuple(value: &PyTuple) -> bool {
     let bases = match value.get_type().getattr("__bases__") {
         Ok(bases) => bases,
-        Err(_) => {
-            println!(" does not have basis");
-            return false;
-        }
+        Err(_) => return false,
     };
     let bases = match bases.cast_as::<PyTuple>() {
         Ok(bases) => bases,
-        Err(_) => {
-            println!("Basis is not a topical");
-            return false;
-        }
+        Err(_) => return false,
     };
     if bases.len() != 1 {
-        println!("Basis is not a singleton");
         return false;
     }
     // TODO: check that bases[0] is tuple
@@ -82,7 +79,7 @@ fn is_namedtuple(value: &PyTuple) -> bool {
     fields.cast_as::<PyTuple>().is_ok()
 }
 
-fn extract_namedtuple(value: &PyTuple) -> Result<ron::Value, PyErr> {
+fn extract_namedtuple(py: Python, value: &PyTuple) -> Result<ron::Value, PyErr> {
     let name = value
         .getattr("__class__")?
         .getattr("__name__")?
@@ -93,8 +90,30 @@ fn extract_namedtuple(value: &PyTuple) -> Result<ron::Value, PyErr> {
         .cast_as::<PyDict>()?
     {
         let name = name.extract::<String>()?;
-        let value = extract(value)?;
+        let value = extract(py, value)?;
         s.insert(name, value);
+    }
+    Ok(ron::Value::Struct(s))
+}
+
+fn extract_dataclass(py: Python, value: &PyAny) -> Result<ron::Value, PyErr> {
+    let name = value
+        .getattr("__class__")?
+        .getattr("__name__")?
+        .extract::<String>()?;
+    let mut s = ron::value::Struct::new(Some(name));
+    // for field in mydataclass.__dataclass_fields__:
+    //   value = getattr(mydataclass, field)
+    //   ..
+    for field in value
+        .getattr("__dataclass_fields__")?
+        .cast_as::<PyDict>()?
+        .keys()
+    {
+        let field = field.extract::<String>()?;
+        let value = value.getattr(&field)?;
+        let value = extract(py, value)?;
+        s.insert(field, value);
     }
     Ok(ron::Value::Struct(s))
 }
--- test.py
+++ test.py
@@ -1,6 +1,33 @@
+from typing import List, Sequence
 import pyron
 from collections import namedtuple
+from dataclasses import dataclass
 
 print(pyron.to_string({"a": 5}))
-print(pyron.to_string([1,2,3,4]))
-print(pyron.to_string(namedtuple("Point", ["x", "y"])(1,2)))
+print(pyron.to_string([1, 2, 3, 4]))
+print(pyron.to_string(namedtuple("Point", ["x", "y"])(1, 2)))
+
+
+@dataclass
+class User:
+    name: str
+    age: int
+
+
+@dataclass
+class QueryResult:
+    users: List[User]
+    count: int
+
+
+print(
+    pyron.to_string(
+        QueryResult(
+            users=[
+                User(name="John", age=30),
+                User(name="Jane", age=25),
+            ],
+            count=2,
+        )
+    )
+)
